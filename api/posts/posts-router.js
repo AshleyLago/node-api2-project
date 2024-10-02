cca1b1e8b90ff7e06ac11d0c78cbbae95de71d48
Ashley Lago <ashl3ylag0@gmail.com>
2024-10-02 16:50:57 -0400

    Working on Task 2, server running successfully

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,18 @@
 // implement your posts router here
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(200).send("GET /posts endpoint. ")
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    res.status(200).send(`GET /posts/${id} endpoint. `)
+})
+
+router.post('/', (req, res) => {
+    res.status(200).send("POST /posts endpoint. ")
+})
+
+module.exports = router
