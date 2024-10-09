9fb038213c8daa0a051b9bcf8d822d619d0821a0
Ashley Lago <ashl3ylag0@gmail.com>
2024-10-09 02:24:25 -0400

    Finished the Project

--- api/posts/posts-model.js
+++ api/posts/posts-model.js
@@ -11,32 +11,44 @@ module.exports = {
   insertComment,
 };
 
+// Calling find returns a promise that resolves to an array of all the posts contained in the database.
 function find() {
   return db('posts');
 }
 
+// This method expects an id as it's only argument and returns a promise that resolves to the post 
+//  corresponding to the id provided or undefined if no post with that id is found.
 function findById(id) {
   return db('posts').where({ id: Number(id) }).first()
 }
 
+// Calling insert passing it a post object will add it to the database and return a promise that 
+//  resolves to an object with the id of the inserted post. The object looks like this: { id: 123 }.
 function insert(post) {
   return db('posts')
     .insert(post)
     .then(ids => ({ id: ids[0] }));
 }
 
+// Accepts two arguments, the first is the id of the post to update and the second is an object 
+//  with the changes to apply. It returns a promise that resolves to the count of updated records. 
+//  If the count is 1 it means the record was updated correctly.
 function update(id, post) {
   return db('posts')
     .where('id', Number(id))
     .update(post);
 }
 
+// The remove method accepts an id as its first argument and upon successfully deleting the post 
+//  from the database it returns a promise that resolves to the number of records deleted.
 function remove(id) {
   return db('posts')
     .where('id', Number(id))
     .del();
 }
 
+// The findPostComments accepts a postId as its first argument and returns a promise that 
+//  resolves to an array of all comments on the post associated with the post id.
 function findPostComments(postId) {
   return db('comments')
     .join('posts', 'posts.id', 'post_id')
--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,12 +3,10 @@ const express = require('express')
 const Posts = require('./posts-model')
 const router = express.Router()
 
-// 1: If successful, returns an array of all the post objects contained
-//  in the database. If not, returns a code of 500 with the message
-//  "The posts information could not be retrieved"
+// 1: If successful, returns an array of all the post objects contained in the database. 
+//  If not, returns a code of 500
 router.get('/', (req, res) => {
-
-    Posts.find(req.query)
+    Posts.find()
         .then(posts => {
             res.status(200).json(posts)
         })
@@ -18,9 +16,8 @@ router.get('/', (req, res) => {
         })
 })
 
-// 2: If successful, returns the post object with the specified id. If
-//  not, returns a code of 404 and the message "The post with the 
-//  specified ID does not exist"
+// 2: If successful, returns the post object with the specified id. If not, returns a 
+//  code of 404
 router.get('/:id', (req, res) => {
     Posts.findById(req.params.id)
         .then(posts => {
@@ -35,8 +32,26 @@ router.get('/:id', (req, res) => {
             res.status(500).json({ message: "The post information could not be retrieved" })
         })
 })
+// Another method may have been changing it to "('/:id', async(req, res) =>" and having 
+//  the body be:
+/***
+try {
+    const post = await Posts.findById(req.params.id)
+    if (post) {
+        res.status(200).json(post)
+    } else {
+        res.status(404).json({ message: ""})
+    }
+} catch (err) {
+    console.log(err)
+    res.status(500).json({ message: ""})
+}
+***/
 
-// 3: If successful, creates a post using the information sent inside the request body and returns the newly created post object
+// 3: If successful, creates a post using the information sent inside the request body 
+//  and returns the newly created post object with the code 201. If the request body 
+//  is missing the title or contents property, return a code of 400. If there's an 
+//  error while saving the post, return a code of 500
 router.post('/', (req, res) => {
     const { title, contents} = req.body
     if (!title || !contents) {
@@ -57,7 +72,11 @@ router.post('/', (req, res) => {
         
 })
 
-// 4: If successful, updates the post with the specified id using data from the request body and returns the modified document, not the original
+// 4: If successful, updates the post with the specified id using data from the request 
+//  body and returns the modified document, not the original, along with the code 200. 
+//  If the post with the specified id is not found, return a code of 404. If the request 
+//  body is missing the title or contents property, return a code of 400. If there's an 
+//  error when updating the post, return a code of 500
 router.put('/:id', (req, res) => {
     const { title, contents} = req.body
     if (!title || !contents) {
@@ -85,11 +104,12 @@ router.put('/:id', (req, res) => {
                 console.log(err)
                 res.status(500).json({ message: "The post information could not be modified" })
             })
-
     }
 })
 
-// 5: If successful, removes the post with the specified id and returns the deleted post object
+// 5: If successful, removes the post with the specified id and returns the deleted post
+//  object. If the post with the specified id is not found, return a code of 404. If 
+//  there's an error in removing the post from the database, return a code of 500
 router.delete('/:id', async(req, res) => {
     try {
         const post = await Posts.findById(req.params.id)
@@ -105,7 +125,9 @@ router.delete('/:id', async(req, res) => {
     }
 })
 
-// 6: If successful, returns an array of all the comment objects associated with the post with the specified id 
+// 6: If successful, returns an array of all the comment objects associated with the post
+//  with the specified id If the post with the specified id is not found, return a code of 
+//  404. If there's an error in removing the post from the database, return a code of 500
 router.get('/:id/comments', async (req, res) => {
     try {
         const post = await Posts.findById(req.params.id)
